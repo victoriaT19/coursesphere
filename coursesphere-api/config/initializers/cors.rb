94090a7fb4671e56575530c98b7231e4184b9ed6
victoria <vtauanny19@gmail.com>
2026-05-11 18:58:09 -0300

    chore: configure deploy for Render and Vercel

--- coursesphere-api/config/initializers/cors.rb
+++ coursesphere-api/config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://course-sphere-phi.vercel.app/", "http://localhost:5173"
+    origins "https://course-sphere-phi.vercel.app", "http://localhost:5173"
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
